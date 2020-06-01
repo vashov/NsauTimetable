ee07f3a00c3aa3dc4d49befef85533215e586b44
Vashov <vashovway@gmail.com>
2020-06-02 04:09:00 +0700

    add deploy to VPS

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script is started on VPS by Github Actions
+
+function writeHeader {
+	echo "----------------------------------------"
+	echo $1
+	echo "----------------------------------------"
+}
+
+writeHeader "Pull actual version"
+git checkout master
+git pull
+
+writeHeader "Build docker for web"
+docker build -t nsau_web
+
+writeHeader "Set environment variables"
+export DB_CONNECTION_STRING=$1
+
+writeHeader "Run docker for web"
+docker run -p 80:8080 -e DB_CONNECTION_STRING -d nsau_web
+
+writeHeader "Check web app"
+curl localhost:80
