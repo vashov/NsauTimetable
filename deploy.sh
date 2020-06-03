372b9aad2682caaee6594efd4ff4b5d71f154c0a
Boris Vashov <39405070+vashov@users.noreply.github.com>
2020-06-03 16:26:11 +0700

    Change script in GitHub actions (#16)
    Change way to deploy. Without docker now.

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,49 @@ function writeHeader {
 	echo "---------------------------------------- $1"
 }
 
-writeHeader "Pull actual version"
-git checkout master
-git pull
+function setEnvVariables {
+	writeHeader "Set environment variables"
+	export DB_CONNECTION_STRING=$1
+}
 
-writeHeader "Build docker for web"
-docker build -t nsau_web -f NsauT.Web/Dockerfile .
+function moveToProjectFolder {
+	writeHeader "Move to project repository folder"
+	cd "~/projects/nsau_timetable"
+}
 
-writeHeader "Set environment variables"
-export DB_CONNECTION_STRING=$1
+function createPublishFolder {
+	writeHeader "Create publish folder"
+	mkdir -p --verbose outputDirectory
+}
 
-writeHeader "Run docker for web"
-docker run -p 80:8080 -e DB_CONNECTION_STRING -d nsau_web
+function cleanPublishFolder {
+	writeHeader "Clean publish folder"
+	local cleanPath="${outputDirectory}/*"
+	rm -r cleanPath
+}
 
-writeHeader "Check web app"
-curl localhost:80
+function buildWebRelease {
+	writeHeader "Build web release"
+	dotnet publish -c Release -o outputDirectory
+}
+
+function runWebRelease {
+	writeHeader "Run web release"
+	local executingPath="${outputDirectory}/NsauT.Web"
+	dotnet executingPath
+}
+
+function checkWebApp {
+	writeHeader "Check web app"
+	curl localhost:5000
+}
+
+outputDirectory="/var/www/nsau_web"
+
+setEnvVariables $1 &&
+moveToProjectFolder && 
+createPublishFolder &&
+cleanPublishFolder && 
+buildWebRelease && 
+runWebRelease && 
+checkWebApp
